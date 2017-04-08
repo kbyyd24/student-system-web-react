ea15bd48b0ab2d9f9f5576d2da21bf1bc91e9017
gaoyx <melo@gaoyuexiang.cn>
2017-04-08 22:10:54 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StudentSystem from './StudentSystem';
+import './index.css';
+
+ReactDOM.render(
+  <StudentSystem />,
+  document.getElementById('root')
+);
