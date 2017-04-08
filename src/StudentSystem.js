ca97c5938d30a87184c8c6beb842b79deeaad58e
gaoyx <melo@gaoyuexiang.cn>
2017-04-08 22:19:00 +0800

    update system

---
+++ src/StudentSystem.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+class StudentSystem extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+export default StudentSystem;
