d2f9c913887b9ca0dc9de86d6928dde4a35d3890
gaoyx <melo@gaoyuexiang.cn>
2017-04-09 10:47:54 +0800

    add Router and Route in StudentSystem

--- src/StudentSystem.js
+++ src/StudentSystem.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar';
+import {Route, Router, Link} from 'react-router';
 
 class StudentSystem extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <Router path={"/"} component={Hompage}>
+          <Route path={"/addStudent"} component={AddStudent}/>
+          <Route path={"/score"} component={Score}/>
+          <Route path={"/goodbye"} component={Goodbye}/>
+        </Router>
       </div>
     )
   }
