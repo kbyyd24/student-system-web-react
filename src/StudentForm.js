a45035e53009c0c59475c71218fac108a510f956
gaoyx <melo@gaoyuexiang.cn>
2017-04-16 15:53:36 +0800

    use undefined to set initial value of input

--- src/StudentForm.js
+++ src/StudentForm.js
@@ -6,16 +6,23 @@ class StudentForm extends Component {
 
   constructor(props) {
     super(props);
-    const name = new InputItem('姓名: ', '', 'text');
-    const studentNumber = new InputItem('学号: ', '', 'text');
-    const nation = new InputItem('民族: ', '', 'text');
-    const classNumber = new InputItem('班级: ', '', 'text');
-    const math = new InputItem('数学成绩: ', NaN, 'number');
-    const chinese = new InputItem('语文成绩: ', NaN, 'number');
-    const english = new InputItem('英语成绩: ', NaN, 'number');
-    const program = new InputItem('编程成绩: ', NaN, 'number');
+    const name = new InputItem('姓名: ', undefined, 'text');
+    const studentNumber = new InputItem('学号: ', undefined, 'text');
+    const nation = new InputItem('民族: ', undefined, 'text');
+    const classNumber = new InputItem('班级: ', undefined, 'text');
+    const math = new InputItem('数学成绩: ', undefined, 'number');
+    const chinese = new InputItem('语文成绩: ', undefined, 'number');
+    const english = new InputItem('英语成绩: ', undefined, 'number');
+    const program = new InputItem('编程成绩: ', undefined, 'number');
     this.status = {inputs: [name, studentNumber, nation, classNumber, math, chinese, english, program]};
   }
+
+  submitStudent = () => {
+    const studentForm = [...document.getElementsByTagName('input')]
+      .map(element => ({[element.name]: element.value}))
+      .reduce((acc, next) => Object.assign(acc, next), {});
+    console.log(studentForm)
+  };
 
   render() {
     const inputItems = this.status.inputs.map(inputItem => (
@@ -32,7 +39,7 @@ class StudentForm extends Component {
             {inputItems}
             <div id="msg" className="alert alert-info"><span/></div>
             <div>
-              <button className="btn btn-success">添加</button>
+              <button className="btn btn-success" onClick={this.submitStudent()}>添加</button>
             </div>
           </div>
         </div>
