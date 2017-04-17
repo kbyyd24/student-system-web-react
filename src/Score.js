b718974cf1e9f6d374d5696994fa1f2153676539
gaoyx <melo@gaoyuexiang.cn>
2017-04-17 11:02:42 +0800

    add input and button in query score

--- src/Score.js
+++ src/Score.js
@@ -3,8 +3,19 @@ import React, {Component} from 'react';
 class Score extends Component{
   render() {
     return (
-      <div>
-        this is score
+      <div className="container">
+        <div className="input-group col-lg-6 div-center">
+          <input type="text" id="stuNumbers" className="form-control"/>
+          <div className="input-group-btn">
+            <button className="btn btn-primary">搜索</button>
+          </div>
+          <div className="input-group-btn">
+            <button className="btn btn-block">清空过滤</button>
+          </div>
+        </div>
+        <div>
+          <div id="classes"></div>
+        </div>
       </div>
     );
   }
